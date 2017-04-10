8bb12da246b1bb7db9c74fd29e73b1b0e85a8eb1
bryce-server <bryceness@hotmail.com>
2017-04-10 21:27:09 +0000

    moved io stuff to io.js so it matches what's in bin/www

---
+++ io.js
@@ -0,0 +1,34 @@
+//this is where the events and stuff comes from
+var io = require('socket.io')();		//empty paren used to have 'http'
+//TODO: init all pixels instead of init an array
+var colors = [];
+
+
+//io.on = when request detected
+//socket.on('disconnect') = when disconnect detected
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('allColors', colors);				//tell everyone what the board looks like before they do anything
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  //this function is for the colors, it saves the input to "colors" and tells everyone 
+  socket.on('colorMessage', function(colorMsg) {
+    colors.push({id: colorMsg['id'], color: colorMsg['color']});
+    console.log(colors);
+    io.emit('allColors', colors);
+  });
+  
+  //this resets everything, in case we need to clear the board
+  socket.on('reset', function(res) {
+	  if(res == "farsi") {
+	  	colors = [];
+	  	io.emit('allColors', colors);	//you need to refresh the page to get this to work.. find a way to reset the colors to grey (the colors only keeps track of the changes, so clearing it won't tell the browser to change them back
+	  }
+  });
+  
+});
+
+module.exports = io;		// REQ'D for socket.io to listen
+
