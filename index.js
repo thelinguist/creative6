78941314fc679ad1c7546932020ed2e36669a076
thelinguist <thelinguist@users.noreply.github.com>
2017-04-10 13:12:41 -0600

    fixed index.js to run with express

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var router = express.Router();
+
+var http = require('http').Server(express);
+
 //this is where the events and stuff comes from
 var io = require('socket.io')(http);
+//TODO: init all pixels instead of init an array
 var colors = [];
 
 /* this is the main page we will serve */
-app.get('/', function(req, res){
+router.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-/*this is for reference. It's a demo of the socketIO stuff */
-app.get('/chat', function(req, res){
-  res.sendFile(__dirname + '/chat.html');
-});
 
 //io.on = when request detected
 //socket.on('disconnect') = when disconnect detected
 io.on('connection', function(socket){
   console.log('a user connected');
-  io.emit('allColors', colors);				//tell everyone what the board looks like before they do anything
+  io.emit('allColors', colors);                         //tell everyone what the board looks like before they do anything
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  });
-
-  //this function is for the chat app
-  socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
   });
 
   //this function is for the colors, it saves the input to "colors" and tells everyone 
@@ -38,13 +32,13 @@ io.on('connection', function(socket){
   
   //this resets everything, in case we need to clear the board
   socket.on('reset', function(res) {
-	  if(res == "farsi") {
-	  	colors = [];
-	  	io.emit('allColors', colors);	//you need to refresh the page to get this to work.. find a way to reset the colors to grey (the colors only keeps track of the changes, so clearing it won't tell the browser to change them back
-	  }
+          if(res == "farsi") {
+                colors = [];
+		io.emit('allColors', colors);   //you need to refresh the page to get this to work.. find a way to reset the colors to grey (the colors only keeps track of the changes, so clearing it won't tell the browser to change them back
+          }
   });
   
 });
-http.listen(3030, function(){
-  console.log('listening on *:3030');
-});
+
+//whatever this line does, don't forget it at the end
+module.exports = router;
