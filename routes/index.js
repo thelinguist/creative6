c276d0de984d3e845512ec93b2a48affa940c309
pwils33 <wilson.kcirtap@gmail.com>
2017-04-11 00:43:24 -0600

    trying another thing

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var io = require('socket.io')(http);
 var colors = [];
 
 /* this is the main page we will serve */
-router.get('/', function(req, res){
+router.get('/*', function(req, res){
     console.log("I'm here...");
   res.sendFile('login.html', {root:"public"});
 });
