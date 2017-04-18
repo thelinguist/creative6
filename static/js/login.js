f2da63de4008c8057cad4847227bc008b9ba3dc5
pwils33 <wilson.kcirtap@gmail.com>
2017-04-17 23:51:19 -0600

    now that things aren't crashing, back to the original problem...

--- static/js/login.js
+++ static/js/login.js
@@ -1,4 +1,5 @@
 function onLoad() {
+    console.log(document.getElementById("message"));
     var message = document.getElementById("message").value;
     if (message !== "nothing") {
         alert(message);
