f4c5e4f1bd5c81037e0f7b9d7e6e6ea617bc669e
bryce-server <bryceness@hotmail.com>
2017-04-18 18:44:12 +0000

    gameCode is the variable to keep track of the game

--- static/js/methods.js
+++ static/js/methods.js
@@ -7,43 +7,32 @@ function onLoad() {
 }
 
 function createGame() {
-    console.log("setting form to create game");
     var value =
-        "<input type=\"text\" name=\"game\" placeholder=\"What would you like to call your game?\"/>\n" +
+        "<input type=\"text\" name=\"gameCode\" placeholder=\"What would you like to call your game?\"/>\n" +
         "<input type=\"submit\" formmethod=\"post\" formaction=\"/create\" value=\"create game\"/>\n" +
         "<input type=\"submit\" onclick=\"setForm()\" value=\"cancel\">";
     document.getElementById("chooseAction").innerHTML = value;
 }
 
 function joinGame() {
-    console.log("setting form to join game");
     var value =
         "<input type=\"submit\" formmethod=\"post\" formaction=\"/join\" value=\"join current game\"/>\n" +
-        "<input type=\"submit\" onclick=\"joinNewGame()\" value=\"join new game\" />" +
-        "<input type=\"submit\" onclick=\"setForm()\" value=\"cancel\">";
+        "<input type=\"submit\" onclick=\"joinNewGame()\" value=\"join new game\" />";
     document.getElementById("chooseAction").innerHTML = value;
 }
 
 function joinNewGame() {
-    console.log("setting form to join new game");
     var value =
-        "<input type=\"text\" name=\"game\" placeholder=\"What game would you like to join?\"/>\n" +
+        "<input type=\"text\" name=\"gameCode\" placeholder=\"What game would you like to join?\"/>\n" +
         "<input type=\"submit\" formmethod=\"post\" formaction=\"/create\" value=\"join game\"/>\n" +
         "<input type=\"submit\" onclick=\"setForm()\" value=\"cancel\">";
     document.getElementById("chooseAction").innerHTML = value;
 }
 
 function setForm() {
-    console.log("setting form to default");
     var value =
         "<input type=\"submit\" onclick=\"joinGame()\" value=\"Join Game\"/>\n" +
         "<input type=\"submit\" onclick=\"createGame()\" value=\"Create New Game\"/>";
     document.getElementById("chooseAction").innerHTML = value;
 }
 
-
-function onMainLoad() {
-    console.log("in on main load");
-    onLoad();
-    setForm();
-}
