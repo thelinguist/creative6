3eb291097ec03764d3a3ddbaed358b8c5d58500c
pwils33 <wilson.kcirtap@gmail.com>
2017-04-17 22:38:43 -0600

    added debug logs, also fixed tabbing because ocd

--- routes.js
+++ routes.js
@@ -1,44 +1,53 @@
 var crypto = require('crypto');
 var express = require('express');
 module.exports = function(app) {
-  var users = require('./controllers/user_controller');
-  app.use('/static', express.static( './static')).
-      use('/lib', express.static( '../lib')
-  );
-  app.get('/', function(req, res){
-    if (req.session.user) {
-      res.render('index');
-    } else {
-      res.redirect('/login');
-    }
-  });
-  app.get('/litebrite', function(req, res){
-    if (req.session.user) {
-      res.render('litebrite');
-    } else {
-      res.redirect('/login');
-    }
-  });
-  app.get('/register', function(req, res){
-    if(req.session.user){
-      res.redirect('/');
-    }
-    res.render('register');
-  });
-  app.get('/login',  function(req, res){
-    if(req.session.user){
-      res.redirect('/');
-    }
-    res.render('login');
-  });
-
-  app.get('/logout', function(req, res){
-    req.session.destroy(function(){
-      res.redirect('/login');
+    var users = require('./controllers/user_controller');
+    app.use('/static', express.static( './static')).
+        use('/lib', express.static( '../lib')
+    );
+    app.get('/', function(req, res){
+        console.log("/ api called");
+        if (req.session.user) {
+            res.render('index');
+        } else {
+            res.redirect('/login');
+        }
     });
-  });
-  app.post('/register', users.register);
-  app.post('/user/update', users.updateUser);
-  app.post('/user/delete', users.deleteUser);
-  app.post('/login', users.login);
+    app.get('/litebrite', function(req, res){
+        console.log("/litebrite api called");
+        if (req.session.user) {
+            res.render('litebrite');
+        } else {
+            console.log("redirecting to /login");
+            res.redirect('/login');
+        }
+    });
+    app.get('/register', function(req, res){
+        console.log("/register api called");
+        if(req.session.user){
+            console.log("redirecting to /");
+            res.redirect('/');
+        }
+        res.render('register');
+    });
+    app.get('/login',  function(req, res){
+        console.log("/login api called");
+        if(req.session.user){
+            console.log("redirecting to /")
+            res.redirect('/');
+        }
+        res.render('login');
+    });
+
+    app.get('/logout', function(req, res){
+        console.log("/logout api called");
+        req.session.destroy(function(){
+            console.log("redirecting to /login");
+            res.redirect('/login');
+        });
+    });
+    app.post('/register', users.register);
+    app.post('/user/update', users.updateUser);
+    app.post('/user/delete', users.deleteUser);
+    app.post('/login', users.login);
 }
